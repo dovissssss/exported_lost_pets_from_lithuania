29618e24ca4a327d957c71799482c097621633ca
dovissssss <jusymen@gmail.com>
2023-06-14 17:29:53 +0300

    visualizations functionality update

--- scripts/config.py
+++ scripts/config.py
@@ -2,9 +2,7 @@ EXPORTED_PETS_DATA = "./data/raw/Prarasti_isvezti_augintiniai.csv"
 MUNICIPALITIES_POPULATION_DATA = "./data/raw/data-table.xlsx"
 
 DATA_FILE_URLS = [
-    "https://data.gov.lt/dataset/293/download/13457/Prarasti_isvezti_augintiniai%202023-04-01%2008.00.00.csv",
+    "https://data.gov.lt/dataset/293/download/13457/Prarasti_isvezti_augintiniai%202023-04-01%2008.00.00.csv"
 ]
 
-DATA_FILES_DESTINATIONS = [
-    "./data/raw/Prarasti_isvezti_augintiniai.csv",
-]
+DATA_FILES_DESTINATIONS = ["./data/raw/Prarasti_isvezti_augintiniai.csv"]
--- scripts/visualize_charts.py
+++ scripts/visualize_charts.py
@@ -1,22 +1,20 @@
 import seaborn as sb
 import matplotlib.pyplot as plt
+import config
 from read_datasets import read_exported_pets_dataset
 from analysis_exported_pets import (
     count_exported_pets_sum_by_year,
     group_exported_pets_by_year,
     count_to_how_many_unique_countries_pets_r_exported,
 )
-import config
 
 
 df = read_exported_pets_dataset(config.EXPORTED_PETS_DATA)  # type: ignore
 
-exported_pets_by_year_n_type = count_exported_pets_sum_by_year(df)
 
-
-def plot_exported_pets_by_year_n_type(exported_pets_by_year_n_type):
+def plot_exported_pets_by_year_n_type():
+    exported_pets_by_year_n_type = count_exported_pets_sum_by_year(df)
     # Plot Lost Pets sum by years, type
-
     sb.set(rc={"figure.figsize": (10, 5)})
 
     sb.barplot(
@@ -25,31 +23,38 @@ def plot_exported_pets_by_year_n_type(exported_pets_by_year_n_type):
         hue=exported_pets_by_year_n_type["pet_type"],
         palette="dark",
     )
+    plt.title("Exported pets by year and type")
+    plt.xlabel("year")
+    plt.ylabel("pet_count")
+    plt.savefig("./data/processed/exported_pets_by_year_n_type.png")
 
 
-plt.savefig("exported_pets_by_year_n_type.png")
-
-pets_by_year = group_exported_pets_by_year(df)
-
-
-def plot_exported_pets_by_year(pets_by_year):
+def plot_exported_pets_by_year():
+    pets_by_year = group_exported_pets_by_year(df)
     # Pandas Plot()
     pets_by_year.plot(
         legend=False,
-        figsize=(5, 3),
+        figsize=(10, 5),
         c="green",
         marker="x",
         title="Exported Pets From Lithuania",
+        xlabel="year",
+        ylabel="pet_count",
     )
-
-    plt.savefig("exported_pets_by_year.png")
-
-
-unique_coutries_by_year = count_to_how_many_unique_countries_pets_r_exported(df)
+    plt.savefig("./data/processed/exported_pets_by_year.png")
 
 
-def plot_unique_countries_count_by_year(unique_coutries_by_year):
+def plot_unique_countries_count_by_year():
+    unique_countries_by_year = count_to_how_many_unique_countries_pets_r_exported(df)
     # Plot count of unique countries by year
-    unique_coutries_by_year.plot.bar(legend=False, figsize=(5, 3))
-
-    plt.savefig("unique_countries_count_by_year.png")
+    unique_countries_by_year.plot(
+        x="year",
+        y="export_country",
+        kind="bar",
+        legend=False,
+        figsize=(10, 6),
+        title="Unique Export Countries Count By Year From Lithuania",
+        xlabel="year",
+        ylabel="countries_count",
+    )
+    plt.savefig("./data/processed/unique_countries_count_by_year.png")
